846d0dce7e19436f8c235c6072a58e6829749f89
Davit Gozalishvili <84373811+squirelJr@users.noreply.github.com>
2022-06-09 20:41:54 +0400

    UPD

--- Pages/Main/Main.js
+++ Pages/Main/Main.js
@@ -1,117 +1,166 @@
 
 
 //burger menu with animations
-let burger = document.getElementsByClassName("burger")[0];
-console.log(burger)
+let burger = document.getElementById("burger");
+
 let ul = document.getElementsByClassName("unorderedList");
+let sections = document.querySelectorAll("section");
 let hr = document.getElementsByClassName("hr");
+
 burger.style.zIndex = "999";
+document.body.zIndex = "1";
+ul[0].style.zIndex = "99";
 
-
+const rotateHalf = [{ transform: 'rotate(90deg)' }];
+const rotateFull = [{ transform: 'rotate(180deg)' }];
+const rotateConfig = { duration: 500, iterations: 1 };
+let startScreen = document.getElementsByClassName("start-screen")
 let bool = false;
 burger.addEventListener("click", function (event) {
   bool = !bool;
   ul[0].classList.toggle("burgerOpen", bool);
   if (bool) {
-    setTimeout(burger.style.webkitTransform = "rotate(90deg) ", 9000)
-
+    burger.style.animationFillMode = "forwards";
+    burger.animate(rotateHalf, rotateConfig);
+    startScreen[0].style.cssText = `backgoround-color:black;opacity:0.4;`;
 
   } else {
-    burger.style.webkitTransform = "rotate(180deg)"
+    burger.animate(rotateFull, rotateConfig)
+    startScreen[0].style.cssText = `backgoround-color:none;opacity:1;`;
+
   }
 
 });
-
+startScreen[0].addEventListener("click", function () {
+  bool = false;
+  burger.animate(rotateFull, rotateConfig)
+  startScreen[0].style.cssText = `backgoround-color:none;opacity:1;`;
+  ul[0].classList.toggle("burgerOpen", bool);
+})
 
 
 //fetch data
 async function getPets() {
   const response = await fetch("../PetsJson.json");
   var data = await response.json();
-  console.log(data)
-  return  data;
+
+  return data;
 }
 
 getPets()
 //drawing ourFriends from fetched Data!
-async function set(){
-  await getPets().then(data=>{
+async function set() {
+  await getPets().then(data => {
     const petImages = document.getElementsByClassName("cat");
     const petNames = document.getElementsByClassName("petName");
-    for(let i=0;i<petImages.length;i++){
-      petImages[i].src= `${data[i].img}`;
-      petNames[i].innerHTML=`${data[i].name}`;
+    for (let i = 0; i < petImages.length; i++) {
+      petImages[i].src = `${data[i].img}`;
+      petNames[i].innerHTML = `${data[i].name}`;
     }
-    
+
   })
 }
 set();
 
 
 //slider right
-let j =1;
+// let j =1;
 
+
+const slideRight = [{ transform: 'translateX(100vw)' }];
+const slideFromLeft = [{ transform: 'translateX(-100vw)' }];
+const slideRightTiming = { duration: 500, iterations: 1 };
 
 let right = document.getElementById("right");
-right.addEventListener("click", function(){
-   getPets().then(data=>{
+right.addEventListener("click", function changeCards() {
+  let cards = document.querySelectorAll(".petCard");
+
+
+  let j = Math.ceil(Math.random() * 5);
+  console.log(j);
+  getPets().then(data => {
     const petImages = document.getElementsByClassName("cat");
     const petNames = document.getElementsByClassName("petName");
-    for(let i=0;i<petImages.length;i++){
+    cards.forEach(element => {
+      element.animate(slideRight, slideRightTiming);
+    })
+    for (let i = 0; i < petImages.length; i++) {
+
       petImages[i].rem = ``;
-      petNames[i].innerHTML=``;
-      petImages[i].src = `${data[i+j].img}`;
-      petNames[i].innerHTML=`${data[i+j].name}`;
+      petNames[i].innerHTML = ``;
+      petImages[i].src = `${data[i + j].img}`;
+      petNames[i].innerHTML = `${data[i + j].name}`;
     }
-j++;
 
-if(j>5){
-  j=5;
-}
 
-console.log("jei"+j);
+
+    // if(j>2){
+    //   j=1;
+    // }
+
+    console.log("jei" + j);
   })
 });
 //j==1;
 //slider left
-let k =9;
+let k = 2;
 let left = document.getElementById("left");
-left.addEventListener("click", function(){
-   getPets().then(data=>{
+left.addEventListener("click", function () {
+
+  let j = Math.ceil(Math.random() * 5);
+  getPets().then(data => {
     const petImages = document.getElementsByClassName("cat");
     const petNames = document.getElementsByClassName("petName");
-    --j;
-    for(let i=0;i<petImages.length;i++){
+
+    let cards = document.querySelectorAll(".petCard");
+    cards.forEach(element => {
+      element.animate(slideFromLeft, slideRightTiming);
+    })
+    for (let i = 0; i < petImages.length; i++) {
       petImages[i].rem = ``;
-      petNames[i].innerHTML=``;
-      petImages[i].src = `${data[i+j].img}`;
-      petNames[i].innerHTML=`${data[i+j].name}`;
+      petNames[i].innerHTML = ``;
+      petImages[i].src = `${data[i + j].img}`;
+      petNames[i].innerHTML = `${data[i + j].name}`;
     }
-
-
-if(j<1){
-  j=1
-}
-
-console.log("kei"+ k)
   })
 });
 
 //PopUp
 let cards = document.querySelectorAll(".petCard");
 console.log(cards);
+let petsSection = document.getElementById("pets")
 let pop = document.createElement("div");
-pop.className="popWindow";
-pop.style.cssText=  `position:absolute; width:300px;height:300px; z-index:999999; background-color:black;`
-async function popUp(){
+pop.className = "popWindow";
+pop.style.cssText = `position:absolute;margin-left:auto;margin-right:auto;left:0;right:0;text-align:center; width:100vw;height:100vh; z-index:999999; background-color:black;opacity:0.5;z-index:1;`
+let modal = document.createElement("div");
+modal.className = "moadl";
+modal.style.cssText = `position:absolute;margin-left:auto;margin-right:auto;left:0;right:0;text-align:center; width:50%;height:50%; z-index:999999; background-color:white;z-Index:22;`
 
-  await getPets().then(data=>{
+let source = "";
+
+
+async function popUp(event) {
+
+  await getPets().then(data => {
     cards.forEach(element => {
-      element.addEventListener("click", function(){
-        document.body.appendChild(pop);
+
+      element.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.body.classList.add("stop-scrolling");
+
+        petsSection.appendChild(pop);
+        petsSection.appendChild(modal);
+        let i = element.childNodes;
+        console.log(i);
+        source = i[3].innerText;
+        console.log(i[3].innerText);
+        let a = data.filter(x => x.name === source)
+        console.log(a);
       })
     });
+
   })
+
 }
 
 popUp();
